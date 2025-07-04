17040dda8ff5d37d07ab0ee41cedc967d93a3f3e
sidesbutgithub <SidneyWai3@gmail.com>
2025-07-04 12:48:51 -0400

    added error handling for bad requests

--- src/index.ts
+++ src/index.ts
@@ -7,22 +7,70 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
-const apiAddress = process.env.API_ADDRESS
-const apiKey = process.env.API_KEY
-const testLocation = process.env.TEST_LOCATION
+const apiAddress = process.env.API_ADDRESS;
+const apiKey = process.env.API_KEY;
 
-//app.use(express.json())
+app.use(express.json());
 
-app.get("/", async (req, res) => {
-    const response = await fetch(`${apiAddress}${testLocation}?key=${apiKey}`);
-    console.log(`${apiAddress}${testLocation}?key=${apiKey}`)
-    const data = await response.json()
+app.get("/:location", async (req, res) => {
+    const {location} = req.params;
 
-    res.status(200).json({"data": data});
+    try{
+        const response = await fetch(`${apiAddress}${location}?key=${apiKey}`);
+        if (!response.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const data = await response.json();
+        res.status(200).json({"data": data});
+    }
+    catch (e){
+        console.log(e)
+        res.status(400).send(e)
+    }
+
+
 });
 
-app.get("/test", (req, res) => {
-    res.send("test success");
+app.get("/:location/:date1", async (req, res) => {
+    const {location, date1} = req.params;
+    
+    try{
+        const response = await fetch(`${apiAddress}${location}/${date1}?key=${apiKey}`);
+        if (!response.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const data = await response.json();
+        res.status(200).json({"data": data});
+    }
+    catch (e){
+        console.log(e)
+        res.status(400).send(e)
+    }
 });
+
+app.get("/:location/:date1/:date2", async (req, res) => {
+    const {location, date1, date2} = req.params;
+    
+    try{
+        const response = await fetch(`${apiAddress}${location}/${date1}/${date2}?key=${apiKey}`);
+        if (!response.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const data = await response.json();
+        res.status(200).json({"data": data});
+    }
+    catch (e){
+        console.log(e)
+        res.status(400).send(e)
+    }
+});
+
+app.get("/", (req, res) => {
+    res.status(400).send("Location is a required route")
+});
+
+app.all("/*splat", (res, req) => {
+    req.status(404).send("Resourse Not Found");
+})
 
 app.listen(port, () => console.log(`listening on port ${port}`));
