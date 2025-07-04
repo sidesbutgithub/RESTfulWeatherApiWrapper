2ff304ecbeb78fdbe6c7ce0ae525eb473e9e1ef4
sidesbutgithub <SidneyWai3@gmail.com>
2025-07-04 09:46:05 -0400

    implemented basic api call

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,22 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
-const apiAddress = process.env.API_ADRESS
+const apiAddress = process.env.API_ADDRESS
 const apiKey = process.env.API_KEY
+const testLocation = process.env.TEST_LOCATION
 
-app.use()
+//app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("received get request");
+app.get("/", async (req, res) => {
+    const response = await fetch(`${apiAddress}${testLocation}?key=${apiKey}`);
+    console.log(`${apiAddress}${testLocation}?key=${apiKey}`)
+    const data = await response.json()
+
+    res.status(200).json({"data": data});
 });
+
 app.get("/test", (req, res) => {
     res.send("test success");
 });
-
 
 app.listen(port, () => console.log(`listening on port ${port}`));
