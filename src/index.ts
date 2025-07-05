4358262b17f74b132f1749a74eeb587235afd81b
sidesbutgithub <SidneyWai3@gmail.com>
2025-07-05 09:18:59 -0400

    added rate limiting

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,23 @@ import express from "express";
 import dotenv from "dotenv";
 import { weatherRouter } from "./routes/weather.js"
 import { createClient } from "redis";
+import rateLimit from "express-rate-limit";
 
-
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, //15minutes = 15*60 seconds, 1000 ms
+    max: 5
+});
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(limiter);
+
 app.locals.cache = null;
 
+//app cannot start listening until connection to cache is established due to the cache automatically trying to reconnection
 const client = await createClient()
     .on("error", (err) => {
         console.log("Redis Client Error, Cache Unavailable", err);
@@ -22,7 +29,6 @@ const client = await createClient()
     .connect();
 
 app.locals.cache = client;
-
 
 app.use(express.json());
 
